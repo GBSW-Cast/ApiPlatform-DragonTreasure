d8b5a4d825568d1f058daf011a3bbdd4be3ff617
Gerald Byrket <gerald@gbsw.net>
2024-06-20 21:44:15 -0500

    Completed Vue Magic with dealing with User data

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
+  /**
+   * @throws ExceptionInterface
+   */
+  #[Route('/', name: 'app_home')]
+    public function index(NormalizerInterface $normalizer,#[CurrentUser] User $user = null): Response
     {
-        return $this->render('home/index.html.twig', [] );
+      return $this->render('home/index.html.twig', [
+        'userData' => $normalizer->normalize($user, 'jsonld', [
+          'groups' => ['user:read'],
+        ]),
+      ]);
     }
 }
